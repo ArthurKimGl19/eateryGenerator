4067ec039e7f01efcb8b2df47305aa097fc84528
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-04-15 12:15:28 -0700

    Update fav icon, add in prettier and eslint, and create basic components

---
+++ src/components/Randomizer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Randomizer(){
+    return (
+        <div>
+            <h1>Randomizer title</h1>
+            <div>Randomizer</div>
+        </div>
+    )
+}
---
+++ src/components/Result.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Result(){
+    return (
+        <div>
+            <h1>Result title</h1>
+            <div>Result</div>
+        </div>
+    )
+}
