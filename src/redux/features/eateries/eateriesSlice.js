f2bbb9c3a6dbe7c4d990e933c34e9149e5d6b924
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-05-09 21:49:21 -0700

    Update eateriesSlice reducers to not allow duplicate choices and allow the user to select more eateries than are available

--- src/redux/features/eateries/eateriesSlice.js
+++ src/redux/features/eateries/eateriesSlice.js
@@ -10,7 +10,6 @@ const cleanupData = function (data) {
     });
     return output;
 };
-
 const initialRandomEatery = {
     name: '',
     type: '',
@@ -25,27 +24,56 @@ const initialState = {
     initialData: data,
     eateries: cleanupData(data),
     randomEatery: initialRandomEatery,
-    history: []
+    history: [],
+    shuffledIndexes: null,
+    noMoreEateries: false
 };
+
+const shuffleEateries = (array) => {
+    // Fisher-Yates shuffle algo
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
 const eateriesSlice = createSlice({
     name: 'eateries',
     initialState,
     reducers: {
         createRandomEatery: (state) => {
-            const min = 0;
-            const max = Object.keys(state.eateries).length;
-            const randomNumber = Math.floor(Math.random() * (max - min) + min);
-            const randomEatery = state.eateries[randomNumber];
-            randomEatery.index = randomNumber;
-            state.randomEatery = randomEatery;
+            let shuffledIndexes = state.shuffledIndexes;
+            if (!shuffledIndexes){
+                shuffledIndexes = shuffleEateries(Object.keys(state.eateries))
+            }
+            if (shuffledIndexes.length > 0){
+                const randomIndex = shuffledIndexes.pop();
+                const randomEatery = state.eateries[randomIndex];
+                randomEatery.index = randomIndex;
+                state.randomEatery = randomEatery;
+                state.shuffledIndexes = [...shuffledIndexes];
+            } else {
+                state.randomEatery = initialRandomEatery;
+            }
         },
         updateHistory: (state) => {
-            const randomEatery = state.randomEatery;
-            state.history.push(randomEatery);
+            if (state.randomEatery.name.length > 0){
+                const randomEatery = state.randomEatery;
+                state.history.push(randomEatery);
+            }
         },
         clearHistory: (state) => {
             state.history = [];
+            state.shuffledIndexes = null;
+            state.noMoreEateries = false
+        },
+        checkIfEateriesAvailable: (state) => {
+            const shuffledIndexes = state.shuffledIndexes;
+            if (shuffledIndexes && shuffledIndexes.length === 0) {
+                state.noMoreEateries = true;
+                state.randomEatery = initialRandomEatery;
+            }
         },
         clearRandomEatery: (state) => {
             state.randomEatery = initialRandomEatery;
@@ -57,7 +85,7 @@ const eateriesSlice = createSlice({
     }
 });
 
-export const { createRandomEatery, updateHistory, clearHistory, clearRandomEatery } =
+export const { createRandomEatery, updateHistory, clearHistory, checkIfEateriesAvailable, clearRandomEatery } =
     eateriesSlice.actions;
 export default eateriesSlice.reducer;
 /* eslint-disable prettier/prettier */
