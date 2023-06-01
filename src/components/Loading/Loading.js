1651bfdbc71aeb10443832d6484af237f0782f4c
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-01 16:45:23 -0700

    Create loading component

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+export default function Loading({ text = 'Loading', speed = 300 }) {
+    const [content, setContent] = React.useState(text);
+
+    React.useEffect(() => {
+        const currentInterval = window.setInterval(() => {
+            setContent((content) => {
+                return content === `${text}...` ? text : `${content}.`;
+            });
+        }, speed);
+        return () => window.clearInterval(currentInterval);
+    }, [text, speed]);
+
+    return <div>{content}</div>;
+}
+
+Loading.propTypes = {
+    text: PropTypes.string,
+    speed: PropTypes.number
+};
