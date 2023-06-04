4b195b20203bebcc5bd0109fe9f9eb58d6b1c35f
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-03 18:14:28 -0700

    Render eateries in eateries component

--- src/components/Eateries/Eateries.js
+++ src/components/Eateries/Eateries.js
@@ -1,7 +1,60 @@
 import React from 'react';
 
 import './Eateries.css';
+import { Container } from 'react-bootstrap';
+import BTable from 'react-bootstrap/Table';
+import { useSelector } from 'react-redux';
 
 export default function Eateries() {
-    return <div>Eateries Component</div>;
+    const eateries = useSelector((state) => state.eateries);
+    const tableHeader = [
+        'number',
+        'name',
+        'type',
+        'rating',
+        'dollar sign',
+        'address',
+        'zip code',
+        'notes'
+    ];
+
+    const calculateDollarSign = (number) => {
+        let result = '';
+        for (let i = 0; i < number; i++) {
+            result += '$';
+        }
+        return result;
+    };
+    return (
+        <Container className="history-container">
+            <h4>Eateries</h4>
+            <BTable striped bordered hover responsive size="sm" className="">
+                <thead>
+                    <tr className="">
+                        {tableHeader.map((header, index) => {
+                            return <td key={index}>{header}</td>;
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(eateries).map((position, index) => {
+                        const { name, type, rating, dollarSign, address, zipCode, note } =
+                            eateries[position];
+                        return (
+                            <tr key={index} className="">
+                                <td>{index + 1}</td>
+                                <td>{name}</td>
+                                <td>{type}</td>
+                                <td>{rating}</td>
+                                <td>{calculateDollarSign(dollarSign)}</td>
+                                <td>{address}</td>
+                                <td>{zipCode}</td>
+                                <td>{note}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </BTable>
+        </Container>
+    );
 }
