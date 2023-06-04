34d36e1b4c677fa7afe970352cecadf81e834937
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-04 13:54:07 -0700

    Add in tooltip regarding distance info for proximity line in Result component

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
 import './Result.css';
 export default function Result({
@@ -26,6 +30,22 @@ export default function Result({
         window.open(`https://maps.google.com?q=${lat},${long}`);
     };
     const { latitude, longitude } = coordinates;
+    const tooltip = (
+        <Tooltip id="tooltip-right">
+            <div>
+                <strong>Close</strong> is less than 5 miles
+            </div>
+            <div>
+                <strong>Moderately Close</strong> is between 5-8 miles
+            </div>
+            <div>
+                <strong>Far</strong> is between 8-15 miles
+            </div>
+            <div>
+                <strong>Very Far</strong> is 15+ miles
+            </div>
+        </Tooltip>
+    );
     if (name) {
         return (
             <Card border="secondary" className="result-card">
@@ -36,7 +56,20 @@ export default function Result({
                     <ul>Dollar Sign: {calculateDollarSign(dollarSign)}</ul>
                     <ul>Address: {address}</ul>
                     <ul>Zip Code: {zipCode}</ul>
-                    {proximity && <ul>Proximity: {proximity}</ul>}
+                    {proximity && (
+                        <ul>
+                            Proximity: {proximity}
+                            <OverlayTrigger
+                                trigger="click"
+                                placement="right"
+                                overlay={tooltip}
+                                defaultShow>
+                                <Button variant="link">
+                                    <FontAwesomeIcon icon={faCircleInfo} />
+                                </Button>
+                            </OverlayTrigger>
+                        </ul>
+                    )}
                     {note && <ul>Notes: {note}</ul>}
                     <Button
                         onClick={() => showDirections(latitude, longitude)}
