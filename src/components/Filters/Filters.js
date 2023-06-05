8f18745960950316291934d95d0c811d72652494
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-04 17:45:20 -0700

    Render Filters component

---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Filters.css';
+
+export default function Filters() {
+    return (
+        <div>
+            <div>Filters</div>
+            <div>Filters Options</div>
+        </div>
+    );
+}
