fa58b8489e41475cda771be3c76e4a65431dbaec
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-05 19:58:26 -0700

    Dry up code by creating createDropdownOptions function

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -12,6 +12,10 @@ export default function Filters() {
     const [prices, setPrices] = React.useState([]);
     const [proximity, setProximity] = React.useState([]);
 
+    const [selectedTypes, setSelectedTypes] = React.useState([]);
+    const [selectedPrices, setSelectedPrices] = React.useState([]);
+    const [selectedProximity, setSelectedProximity] = React.useState([]);
+
     const eateries = useSelector((state) => state.eateries);
 
     const createOptions = function (name, updateState) {
@@ -32,35 +36,48 @@ export default function Filters() {
         createOptions('proximity', setProximity);
     }, [eateries]);
 
-    const typeOptions = types.map((type, index) => (
-        <Dropdown.Item key={index} eventKey={type} className="type-options">
-            {type}
-        </Dropdown.Item>
-    ));
-    const priceOptions = prices.map((dollarSign, index) => {
-        const formattedDollarSign = calculateDollarSign(dollarSign);
-        return (
-            <Dropdown.Item key={index} eventKey={formattedDollarSign} className="price-options">
-                {formattedDollarSign}
-            </Dropdown.Item>
-        );
-    });
-    const proximityOptions = proximity.map((proximity, index) => (
-        <Dropdown.Item key={index} eventKey={proximity} className="proximity-options">
-            {proximity}
-        </Dropdown.Item>
-    ));
+    const createDropdownOptions = (items, className) => {
+        return items.map((item, index) => {
+            if (className === 'price-options') {
+                const formattedDollarSign = calculateDollarSign(item);
+                return (
+                    <Dropdown.Item key={index} eventKey={formattedDollarSign} className={className}>
+                        {formattedDollarSign}
+                    </Dropdown.Item>
+                );
+            } else {
+                return (
+                    <Dropdown.Item key={index} eventKey={item} className={className}>
+                        {item}
+                    </Dropdown.Item>
+                );
+            }
+        });
+    };
+
+    const typeOptions = createDropdownOptions(types, 'type-options');
+    const priceOptions = createDropdownOptions(prices, 'price-options');
+    const proximityOptions = createDropdownOptions(proximity, 'proximity-options');
 
     return (
         <Container className="filters-container">
             <h6>Filters</h6>
-            <DropdownButton title="Type" className="dropdown-button">
+            <DropdownButton
+                title="Type"
+                className="dropdown-button"
+                onSelect={(evt) => console.log('types ', evt)}>
                 {typeOptions}
             </DropdownButton>
-            <DropdownButton title="Price" className="dropdown-button">
+            <DropdownButton
+                title="Price"
+                className="dropdown-button"
+                onSelect={(evt) => console.log('prices ', evt)}>
                 {priceOptions}
             </DropdownButton>
-            <DropdownButton title="Proximity" className="dropdown-button">
+            <DropdownButton
+                title="Proximity"
+                className="dropdown-button"
+                onSelect={(evt) => console.log('proximity ', evt)}>
                 {proximityOptions}
             </DropdownButton>
         </Container>
