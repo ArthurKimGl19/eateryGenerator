600000f8e575cf8d1339e81d5c84b88dc80d2c9d
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-10 22:08:48 -0700

    Format files

--- src/data/convertCsvToJson.js
+++ src/data/convertCsvToJson.js
@@ -5,34 +5,34 @@ const path = require('path');
 const eateryData = path.join(__dirname, 'data-eatery.csv');
 const favoriteData = path.join(__dirname, 'favorite-eatery.csv');
 
-const createFile =  (fileName, data) => {
-  const jsonData = [];
+const createFile = (fileName, data) => {
+    const jsonData = [];
 
-  fs.createReadStream(data)
-    .pipe(csv())
-    .on('data', (row) => {
-      const [latitude, longitude] = row.coordinates
-        .split(',')
-        .map((coord) => parseFloat(coord.trim()));
-      const jsonRow = {
-        name: row.name,
-        type: row.type,
-        rating: parseInt(row.rating),
-        price: Number(row.price),
-        address: row.address,
-        zipCode: Number(row.zipCode),
-        note: row.note,
-        latitude,
-        longitude
-      };
-      jsonData.push(jsonRow);
-    })
-    .on('end', () => {
-      const jsonFilePath = `./src/data/${fileName}.json`; // Replace with the desired path for the output JSON file
-      fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
-      console.log(`${fileName} CSV to JSON conversion completed successfully!`);
-    });
-}
+    fs.createReadStream(data)
+        .pipe(csv())
+        .on('data', (row) => {
+            const [latitude, longitude] = row.coordinates
+                .split(',')
+                .map((coord) => parseFloat(coord.trim()));
+            const jsonRow = {
+                name: row.name,
+                type: row.type,
+                rating: parseInt(row.rating),
+                price: Number(row.price),
+                address: row.address,
+                zipCode: Number(row.zipCode),
+                note: row.note,
+                latitude,
+                longitude
+            };
+            jsonData.push(jsonRow);
+        })
+        .on('end', () => {
+            const jsonFilePath = `./src/data/${fileName}.json`; // Replace with the desired path for the output JSON file
+            fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
+            console.log(`${fileName} CSV to JSON conversion completed successfully!`);
+        });
+};
 
 createFile('data-eatery', eateryData);
 createFile('favorite-eatery', favoriteData);
