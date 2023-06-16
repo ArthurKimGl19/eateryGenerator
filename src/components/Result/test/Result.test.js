54fc0311d0c067fadd757944c840ec83cd7a1e9b
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-15 20:30:25 -0700

    Add in tests for result component

---
+++ src/components/Result/test/Result.test.js
@@ -0,0 +1,51 @@
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithProviders } from '../../../utils/test-utils';
+
+import Result from '../../Result/Result';
+import Eateries from '../../Eateries/Eateries';
+import { cleanupData } from '../../../redux/helpers/eateriesFunctions';
+
+const latitude = '1';
+const longitude = '-1';
+const initialState = {
+    address: 'example address',
+    name: 'example eatery',
+    note: 'example note',
+    price: 1,
+    rating: 1,
+    type: 'example type',
+    zipCode: 90001,
+    coordinates: { latitude, longitude }
+};
+
+const initialEmptyState = {
+    name: '',
+    type: '',
+    rating: 0,
+    price: 0,
+    zipCode: 0,
+    address: '',
+    note: '',
+    index: null,
+    proximity: '',
+    coordinates: { latitude: 0, longitude: 0 }
+};
+
+describe('Successfully render result component', () => {
+    test('Render a result if a result is given', async () => {
+        renderWithProviders(<Result {...initialState} />);
+        const eatery = await screen.findByText(/example eatery/i);
+        expect(eatery).toBeInTheDocument();
+    });
+
+    test('Render no result if data is empty', async () => {
+        renderWithProviders(<Result {...initialEmptyState} />);
+        const type = await screen.queryByText(/type/i);
+        expect(type).not.toBeInTheDocument();
+        const rating = await screen.queryByText(/rating/i);
+        expect(rating).not.toBeInTheDocument();
+        const price = await screen.queryByText(/price/i);
+        expect(price).not.toBeInTheDocument();
+    });
+});
