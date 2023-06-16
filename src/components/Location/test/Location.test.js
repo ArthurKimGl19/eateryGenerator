605a7ca734d739a91b750bbf101cc46000346236
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-06-16 15:01:50 -0700

    Add tests for location component

---
+++ src/components/Location/test/Location.test.js
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react';
+
+import Location from '../Location';
+import { renderWithProviders } from '../../../utils/test-utils';
+
+const initialGeolocation = {
+    coordinates: {
+        latitude: 0,
+        longitude: 0
+    },
+    loading: true,
+    error: null
+};
+
+describe('Successfully renders location component', () => {
+    test('Renders the location component', async () => {
+        renderWithProviders(<Location />, {
+            preloadedState: {
+                geolocation: initialGeolocation
+            }
+        });
+
+        const initialText = await screen.findByText(/finding location/i);
+        expect(initialText).toBeInTheDocument();
+    });
+});
