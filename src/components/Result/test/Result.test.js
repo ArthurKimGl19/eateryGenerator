c97fa1498bf6b3de3da4f8308e433084d5792466
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-07-03 16:59:36 -0700

    Update Result tests to be able to handle updated redux and test-util files

--- src/components/Result/test/Result.test.js
+++ src/components/Result/test/Result.test.js
@@ -1,33 +1,64 @@
+import React from 'react';
 import { act, screen } from '@testing-library/react';
 
 import Result from '../../Result/Result';
 import { renderWithProviders } from '../../../utils/test-utils';
 import userEvent from '@testing-library/user-event';
 
+// if (!(global as any).window) {
+//     (global as any).window = Object.create(window);
+// }
 global.window ??= Object.create(window);
 Object.defineProperty(window, 'open', { value: jest.fn() });
+
+// interface InitialState {
+//     name: string;
+//     type: string;
+//     rating: number;
+//     price: number;
+//     address: string;
+//     zipCode: number;
+//     note?: string;
+//     proximity?: string;
+//     latitude: number;
+//     longitude: number;
+// }
+//
+// interface InitialEmptyState {
+//     name: string;
+//     type: string;
+//     rating: number;
+//     price: number;
+//     address: string;
+//     zipCode: number;
+//     note?: string;
+//     proximity?: string;
+//     coordinates: {
+//         latitude: number;
+//         longitude: number;
+//     };
+// }
 
 const initialState = {
     name: 'example eatery 1',
     type: 'eatery type 1',
     rating: 1,
     price: 1,
-    zipCode: 1,
     address: 'example address 1',
+    zipCode: 1,
     note: 'example note 1',
     proximity: 'close',
-    latitude: '1',
-    longitude: '-1'
+    latitude: 1,
+    longitude: -1
 };
 const initialEmptyState = {
     name: '',
     type: '',
     rating: 0,
     price: 0,
-    zipCode: 0,
     address: '',
+    zipCode: 0,
     note: '',
-    index: null,
     proximity: '',
     coordinates: { latitude: 0, longitude: 0 }
 };
@@ -36,7 +67,9 @@ describe('Successfully renders result component', () => {
     test('Renders a result if a result is given', async () => {
         renderWithProviders(<Result />, {
             preloadedState: {
-                randomEatery: initialState
+                eateries: {
+                    randomEatery: initialState
+                }
             }
         });
 
@@ -47,8 +80,9 @@ describe('Successfully renders result component', () => {
     test('If result is present, clicking the directions button triggers showDirection', async () => {
         renderWithProviders(<Result />, {
             preloadedState: {
-                randomEatery: initialState
-            }
+                eateries: {
+                    randomEatery: initialState
+                }            }
         });
 
         const button = screen.getByRole('button', { name: /directions/i });
@@ -66,7 +100,9 @@ describe('Successfully renders result component', () => {
     test('Renders no result if data is empty', async () => {
         renderWithProviders(<Result />, {
             preloadedState: {
-                randomEatery: initialEmptyState
+                eateries: {
+                    randomEatery: initialEmptyState
+                }
             }
         });
 
