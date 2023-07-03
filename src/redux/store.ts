6aa7edfdcfede443d27d106d6d0d4c83c9e09d17
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-07-02 21:40:29 -0700

    Refactor test utils, redux store and hooks files

---
+++ src/redux/store.ts
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import { configureStore, combineReducers, PreloadedState } from '@reduxjs/toolkit';
+import eateriesReducer from './features/eateries/eateriesSlice';
+import { loadState, saveState } from '../helpers/localStorageFunctions';
+
+const rootReducer = combineReducers({
+  eateries: eateriesReducer
+})
+export function setupStore(preloadedState?: PreloadedState<RootState>) {
+  const store = configureStore({
+    reducer: eateriesReducer,
+    preloadedState: loadState()
+  });
+  // Can still subscribe to the store
+  store.subscribe(() => {
+    console.log('redux store data', store.getState());
+    saveState(store.getState());
+  });
+  return store;
+}
+
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import { render } from '@testing-library/react';
+import type { RenderOptions } from '@testing-library/react';
 import { configureStore } from '@reduxjs/toolkit';
+import type { PreloadedState } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+
+import type { AppStore, RootState } from '../redux/store';
 // As a basic setup, import your same slice reducers
 import eateriesReducer from '../redux/features/eateries/eateriesSlice';
 
+// This type interface extends the default options for render from RTL, as well
+// as allows the user to specify other things such as initialState, store.
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+    preloadedState?: PreloadedState<RootState>;
+    store?: AppStore;
+}
+
 export function renderWithProviders(
-    ui,
+    ui: React.ReactElement,
     {
         preloadedState = {},
         // Automatically create a store instance if no store was passed in
         store = configureStore({ reducer: eateriesReducer, preloadedState }),
         ...renderOptions
-    } = {}
+    }: ExtendedRenderOptions = {}
 ) {
-    function Wrapper({ children }) {
+    function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
         return <Provider store={store}>{children}</Provider>;
     }
 
