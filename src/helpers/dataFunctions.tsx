248c720cd201123886110d8976b68ecc62946a68
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2023-07-04 17:41:09 -0700

    Cleanup files

--- src/helpers/dataFunctions.tsx
+++ src/helpers/dataFunctions.tsx
@@ -1,4 +1,4 @@
-import { EateryInterface, EateriesInterface } from '../shared/interfaces/eatery.interface'
+import { EateryInterface, EateriesInterface } from '../shared/interfaces/eatery.interface';
 
 export const cleanUpData = function (object: EateriesInterface): EateryInterface[] {
     return Object.keys(object).map((id) => object[id]);
